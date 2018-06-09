bd3bb0db905e87d6e59e894146a652fd109d0801
uulltt <ajvark@gmail.com>
2018-06-09 19:29:46 -0400

    Adding Vertical concat function
    needed to modify this code for my own purposes and thought I'd share my changes here.

--- index.js
+++ index.js
@@ -25,6 +25,28 @@ function concatImages(options, callback) {
 
 };
 
+function concatImagesV(options, callback) {
+
+  var images = Array.isArray(options) ? options : options.images;
+  var margin = (typeof options.margin === 'number') ? options.margin : 10;
+
+  loadImages(images, function(err, images) {
+    if(err) return callback(err);
+    var canvas;
+
+    try {
+      canvas = drawV(images, { margin: margin });
+    } catch(e) {
+      return callback(e);
+    }
+
+    return callback(null, canvas);
+
+  });
+
+
+};
+
 
 function loadImages(images, callback) {
   async.map(images, function(src, done) {
@@ -62,6 +84,34 @@ function draw(images, options) {
 
 }
 
+function drawV(images, options) {
+
+  var margin = options.margin;
+
+  var totalHeight  = (images.length - 1) * margin;
+  var maxWidth  = 0;
+
+  for (var i = 0; i < images.length; i++) {
+    totalHeight  += images[i].height;
+    maxWidth   = (images[i].width > maxWidth) ? images[i].width : maxWidth;
+  };
+
+  var canvas = new Canvas(maxWidth, totalHeight);
+  var ctx = canvas.getContext('2d');
+  var currentLeftMargin = 0;
+
+  for (var i = 0; i < images.length; i++) {
+    currentLeftMargin = (currentLeftMargin === 0) ? currentLeftMargin : currentLeftMargin + margin;
+    ctx.drawImage(images[i], 0, currentLeftMargin);
+    currentLeftMargin += images[i].height;
+  };
+
+  return canvas;
+
+}
+
 module.exports            = concatImages;
+module.exports.h		  = concatImages;
+module.exports.v		  = concatImagesV;
 module.exports.draw       = draw;
 module.exports.loadImages = loadImages;
